5014f7035a1f18b17aeafb58e6a7045e69e9072c
SardineFish <Sardine@live.cn>
2018-06-03 09:25:44 +0800

    Add enemy dead effect

---
+++ Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour {
+    public float DestroyTime = 1;
+    float startTime = 0;
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time - startTime >= DestroyTime)
+            Destroy(gameObject);
+	}
+}
