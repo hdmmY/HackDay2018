7d0bce04d57f9e5b6ff2ca980bb963fd84af1e81
hdmmY <2439846178@qq.com>
2018-06-03 11:09:08 +0800

    Finish connection light prefab

---
+++ Assets/Scripts/ConnectionLightScaleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent (typeof (MeshRenderer))]
+public class ConnectionLightScaleController : MonoBehaviour
+{
+    public float ScaleXFactor;
+
+    private Material _connectLightMat;
+
+    private void OnEnable ()
+    {
+        _connectLightMat = GetComponent<MeshRenderer> ().material;
+    }
+
+    private void Update ()
+    {
+        _connectLightMat.SetFloat ("_ScaleX", transform.localScale.x * ScaleXFactor);
+    }
+
+}
