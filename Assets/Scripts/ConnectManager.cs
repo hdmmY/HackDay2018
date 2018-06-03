6d795a65d7a6f5a7a908ae98ce62859319079d72
SardineFish <Sardine@live.cn>
2018-06-03 11:09:02 +0800

    Add CoonectionManager

---
+++ Assets/Scripts/ConnectManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectManager : Singleton<ConnectManager>
+{
+    public Dictionary<GameObject, Dictionary<GameObject, GameObject>> ConnectionMap = new Dictionary<GameObject, System.Collections.Generic.Dictionary<GameObject, GameObject>>();
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Connect(GameObject start, GameObject end)
+    {
+        GameObject key = null;
+        if (ConnectionMap.ContainsKey(start))
+            key = start;
+        else if (ConnectionMap.ContainsKey(end))
+            key = end;
+        if (key == null)
+            key = start;
+        if(!ConnectionMap.ContainsKey(key))
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/TowerEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerEffect : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameManager.Instance.Player;
+        var dst = (player.transform.position - transform.position).magnitude;
+        if (dst <= GetComponent<TowerProperty>().LightRadius)
+        {
+
+        }
+    }
+}
