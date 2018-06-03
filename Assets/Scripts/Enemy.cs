91693c2bd7524cdefe08db1a53426859d2e20fb8
SardineFish <Sardine@live.cn>
2018-06-03 08:13:15 +0800

    Complete enemy wander AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -21,11 +21,14 @@ public class Enemy : Entity {
     public float MaxSpeed = 5;
     public float MaxRotateSpeed = 600;
     public float MinRotateSpeed = 160;
+    public float DetectRadiusMultiple = 2;
     float numbTime = 0;
+    float currentTargetSpeed = 0;
+    float currentDetcetRadiusMultiple = 1;
 
     private void Awake()
     {
-        StateMachine = StateMachine<EnemyState>.Initialize(this, EnemyState.Idle);
+        StateMachine = StateMachine<EnemyState>.Initialize(this, EnemyState.Wander);
         MoveSpeed = 0;
     }
     // Use this for initialization
@@ -36,7 +39,7 @@ public class Enemy : Entity {
     {
         if(dir.magnitude>0)
         {
-            MoveSpeed = Mathf.Clamp(MoveSpeed + Acceleration * Time.deltaTime, 0, MaxSpeed);
+            MoveSpeed = Mathf.Clamp(MoveSpeed + Acceleration * Time.deltaTime, 0, currentTargetSpeed);
         }
         else
         {
@@ -58,6 +61,18 @@ public class Enemy : Entity {
         }
     }
 
+    public bool Detect()
+    {
+        var player = GameManager.Instance.Player.GetComponent<PlayerProperty>();
+        var dist = (player.transform.position - transform.position).magnitude;
+        if (dist > player.DetectRadius*currentDetcetRadiusMultiple)
+            return false;
+        var hits = Physics2D.RaycastAll(transform.position, player.transform.position - transform.position, dist, 1 << 8);
+        if (hits.Length <= 0)
+            return true;
+        return false;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && StateMachine.State == EnemyState.Chase)
@@ -71,20 +86,22 @@ public class Enemy : Entity {
 
     void Idle_Update()
     {
-        var player = GameManager.Instance.Player.GetComponent<PlayerProperty>();
-        var dist = (player.transform.position - transform.position).magnitude;
-        if (dist > player.DetectRadius)
-            return;
-        var hits = Physics2D.RaycastAll(transform.position,player.transform.position - transform.position, dist, 1 << 8);
-        if (hits.Length <= 0)
+        if (Detect())
             StateMachine.ChangeState(EnemyState.Chase);
+    }
+    
+    void Chase_Enter()
+    {
+        currentDetcetRadiusMultiple = DetectRadiusMultiple;
     }
 
     void Chase_Update()
     {
         var player = GameManager.Instance.Player;
+        currentTargetSpeed = MaxSpeed;
         Aim(player.transform.position - transform.position);
         Move(LookAt);
+        
     }
 
     void Numb_Enter()
@@ -97,8 +114,32 @@ public class Enemy : Entity {
         if (Time.time - numbTime > NumbTime)
             StateMachine.ChangeState(EnemyState.Idle);
     }
-    IEnumerator Wander_Update()
+    IEnumerator Wander_Coroutine()
     {
-
+        while(true)
+        {
+            var nextChangeTime = Time.time + Random.value * 5 + 1;
+            Debug.Log(nextChangeTime);
+            var dir = Random.insideUnitCircle;
+            var speed = Random.Range(0, 0.5f);
+            if (speed < 0.2)
+                speed = 0;
+            while(Time.time<nextChangeTime)
+            {
+                currentTargetSpeed = speed;
+                Move(dir);
+                Aim(dir);
+                yield return null;
+            }
+        }
+    }
+    IEnumerator wander;
+    void Wander_Enter()
+    {
+        wander = Wander_Coroutine();
+    }
+    void Wander_Update()
+    {
+        wander.MoveNext();
     }
 }
